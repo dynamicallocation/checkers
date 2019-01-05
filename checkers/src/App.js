b9237da1254f1797cb4c96158b06171e4ff345f1
Ivan Kavanagh <ivankavanagh@Ivans-MacBook-Pro.local>
2019-01-05 17:09:08 -0600

    created board

---
+++ checkers/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container, Row, Col } from 'react-grid-system';
+
+class App extends Component {
+  render() {
+    return (
+
+
+        <Board/>
+
+    );
+  }
+}
+
+
+function Board(props)
+{
+  return(
+    <div className={"flex-container"}>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+        <div className={"even-square"}/>
+        <div className={"odd-square"}/>
+
+    </div>
+
+);
+}
+
+
+
+export default App;
